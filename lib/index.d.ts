d99a38b075b7ac364424d05dceffd4c23dcc3ded
okunishinishi <okunishinishi@gmail.com>
2019-08-22 16:38:47 +0900

    add onOverride options

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
-type overlay<T> = (overlayer: {}, options?: {autoConvertNumeric?: boolean}) => T
+type overlay<T> = (overlayer: {}, options?: {autoConvertNumeric?: boolean, onOverride?: (obj) => void}) => T
 
 type consts = <T extends {}>(obj: T) => { overlay: overlay<T> }
 
